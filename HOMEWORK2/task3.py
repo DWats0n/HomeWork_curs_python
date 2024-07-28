8577fcec890e67413eb9b9e9f81f2e9fc4bbeb80
DWats0n <67840763+DWats0n@users.noreply.github.com>
2024-07-28 13:28:11 +0300

    Update task3.py

--- HOMEWORK2/task3.py
+++ HOMEWORK2/task3.py
@@ -1,7 +1,6 @@
-
+from io import TextIOWrapper
 
 def open_files():
-  global file_in_1, file_in_2, file_out
   try:
     file_in_1 = open("input1.txt").readlines()
     file_in_2 = open("input2.txt").readlines()
@@ -9,16 +8,24 @@ def open_files():
     print("Find file haven't been finding :(")
     exit()
   file_out = open("output.txt","w")
+  return file_in_1,file_in_2,file_out
 
-def add_linse():
-  pass
+def add_linse(file_in_1,file_in_2):
+  list_out = []
+  
+  for id in range(len(file_in_1)):
+    list_out.append( str(file_in_1[id]).strip() + " " + str(file_in_2[id]).strip() )
 
-def close_file():
-  pass
+  return list_out
+
+def close_file(file_out : TextIOWrapper,list_out):
+  for line in list_out:
+    file_out.write(line+"\n")
+  file_out.close()
 
 def __main__():
-  open_files()
-  add_linse()
-  close_file()
+  file_in_1,file_in_2,file_out = open_files()
+  list_out = add_linse(file_in_1,file_in_2)
+  close_file(file_out,list_out)
   
 __main__()
